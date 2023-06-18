df8a8e058aa1624d3b28f5bf830223125141a7c8
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-18 15:01:22 +0300

    🚀feat(models) add Role model and relationship with User model\n⚙️chore(migrations) create roles and role_user tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
