0e8aa8537e2be3701c97317c1abc6c79b734e4cc
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-18 15:07:11 +0300

    ⚙️chore(travel-model) add tours relationship method

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
     use HasFactory, HasUuids;
 
     protected $table = 'travels';
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
 }
