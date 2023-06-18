81e94d871d1d0c6749254319fa874fded741a9e6
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-18 17:46:05 +0300

    🚀feat(api): add tours endpoint for specific travel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('travels', [TravelController::class, 'index']);
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
