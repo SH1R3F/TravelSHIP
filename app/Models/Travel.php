aa5127cc6d61cc5892978d18c8cc527b7eb91d46
Mahmoud Shiref <sh1r3f@gmail.com>
2023-06-18 16:12:20 +0300

    ⚙️chore(models/travel) add numberOfNights accessor method

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -14,6 +15,9 @@ class Travel extends Model
 
     protected $table = 'travels';
 
+    /**
+     * Relationship to the tours it has
+     */
     public function tours(): HasMany
     {
         return $this->hasMany(Tour::class);
@@ -31,5 +35,15 @@ class Travel extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    /**
+     * Accessor for number_of_nights
+     */
+    protected function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, array $attributes) => $attributes['number_of_days'] - 1
+        );
     }
 }
